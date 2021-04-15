36fd641f13a56bf73e17ea960a88ae3ff3e2a999
cyberbiont <cyberbiont@gmail.com>
2021-04-15 03:39:45 +0300

    improve errors handling

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -28,111 +28,92 @@ export default class Errors {
 
 	public VpError = class VpError extends Error {
 		readonly name = this.constructor.name;
-		constructor(public rootThis: Errors, public message: string = ``) {
+		constructor(public rootThis: Errors, message: string = ``) {
 			super(message);
 			this.rootThis.channel.appendLine(message);
 		}
 	}.bind(null, this);
 
 	public InteractionError = class InteractionError extends this.VpError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `User hasn't provided input.`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public SwapperSymlinkError = class SwapperSymlinkError extends this.VpError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `It seems that thare's a problem with "extensions" symlink`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public BrokenSymlink = class BrokenSymlinkError extends this
 		.SwapperSymlinkError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `It seems that "extensions" symlink is broken`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public MissingSymlink = class MissingSymlinkError extends this
 		.SwapperSymlinkError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `It seems that "extensions" symlink is missing (or the folder is wrongly named).`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public IsDirectory = class IsDirectoryError extends this.SwapperSymlinkError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `It seems that there is a normal directory in place of "extensions" symlink.`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public SymlinkExists = class SymlinkExistsError extends this
 		.SwapperSymlinkError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `It seems that "extensions" symlink already exists and points to this folder.`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public MissingProfilesFolder = class MissingProfilesFolderError extends this
 		.VpError {
-		readonly name = this.constructor.name;
 		constructor(
-			public rootThis: Errors,
-			public message = ``,
+			message = ``,
 			public description = `Profile folder was not found.`,
 		) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	public NoProfiles = class NoProfilesError extends this.VpError {
-		readonly name = this.constructor.name;
-		constructor(
-			public rootThis: Errors,
-			public message = ``,
-			public description = `No profiles found.`,
-		) {
+		constructor(message = ``, public description = `No profiles found.`) {
 			super(`${description} ${message}`);
 			this.rootThis.channel.appendLine(`${description} ${message}`);
 		}
-	}.bind(null, this);
+	};
 
 	// !🕮 <cyberbiont> bbef93f9-e41c-4dea-aaf8-f3010cdbc0c6.md
 	instanceOfNodeError<T extends new (...args: any) => Error>(
